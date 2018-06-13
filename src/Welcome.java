33f602daace33802a5a27672f9be74ff89022613
dipalparmar <dipalparmar13@gmail.com>
2018-06-13 16:51:30 +0530

    login gui

---
+++ src/Welcome.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+class Welcome extends JFrame
+{
+  Welcome()
+  {
+    setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+    setTitle("Welcome");
+     setSize(400, 200);
+  }
+ }
